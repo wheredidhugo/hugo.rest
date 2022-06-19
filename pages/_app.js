bf8cdb42784fc7cb633ee5bbc68c44f5e7a1102c
Hugo <wheredidhugo@protonmail.com>
2022-06-19 23:40:29 +0200

    feat: added info and changed favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { ThemeProvider } from "next-themes";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
@@ -5,12 +6,19 @@ import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class">
-      <div className="p-7 bg-white dark:bg-zinc-900 text-black  dark:text-white h-screen text-center sm:text-left">
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="theme-color" content="#2631fd" />
+        <meta name="title" content="hugo" />
+        <meta name="description" content="@wheredidhugo's homepage" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <div className="p-7 bg-white dark:bg-zinc-900 text-black  dark:text-white h-screen text-center sm:text-left">
+          <Navbar />
+          <Component {...pageProps} />
+          <Footer />
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
