efd20b1383a6491715d56e6c3e2e1727726106bf
Hugo <wheredidhugo@protonmail.com>
2022-06-19 23:01:26 +0200

    feat: made footer more readable and stylish + deleted useless api

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -3,8 +3,8 @@ import FooterIcon from "./Icon";
 
 export default function Footer() {
   return (
-    <div className="text-5xl text-center">
-      <div className="rounded border border-zinc-300 dark:border-zinc-800 my-3"></div>
+    <div className="text-5xl text-center mt-5">
+      <div className="rounded border border-zinc-300 dark:border-zinc-700 my-3"></div>
       <div className="not-last:mr-5 child:inline">
         <FooterIcon
           icon={<FaGithub />}
--- components/Footer/Icon.js
+++ components/Footer/Icon.js
@@ -4,7 +4,7 @@ export default function FooterIcon({ icon, link }) {
       href={link}
       target="_blank"
       rel="noreferrer"
-      className="opacity-30 dark:opacity-10 hover:opacity-100 dark:hover:opacity-100 cursor-pointer"
+      className="opacity-30 dark:opacity-20 hover:opacity-100 dark:hover:opacity-100 cursor-pointer"
     >
       {/* I don't know why I need a dark:hover:opacity-100 */}
       {icon}
